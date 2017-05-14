2102c53e95c0119b0c58f328b22b8f541362431b
jedi-tester <jedi.tester01@gmail.com>
2017-05-14 10:43:48 +0530

    updated JavaDoc comments

--- src/main/java/io/vodqa/extreportng/utils/SystemInfo.java
+++ src/main/java/io/vodqa/extreportng/utils/SystemInfo.java
@@ -7,15 +7,14 @@ import java.util.Map;
  */
 
 /**
- * This interface is for the user who should implement this interface and generate their own system information
- * and return back the map
+ * Implement this interface to generate custom system information
+ * and return back the {@link Map<String, String>} object.
  */
 public interface SystemInfo {
 
     /**
-     * Add a map of system information and return as a map
-     * @return The map of system information
+     *
+     * @return The {@link Map<String, String>} object of system information
      */
-//    Map<String, String> getSystemInfoMap();
     Map<String, String> getSystemInfoMap(String filePath);
 }
